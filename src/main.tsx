46a4afe84ecb57796a1a18ff1621f32e87e7b1a8
sungpaks <emforhs03150@gmail.com>
2025-08-09 14:27:07 +0900

    feat: OpenAPI 전역설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
 import './styles/input.css';
+import { OpenAPI } from './api/core/OpenAPI';
 
+/* OpenAPI 전역설정 */
+OpenAPI.BASE = import.meta.env.VITE_API_BASE_URL;
+// OpenAPI.WITH_CREDENTIALS = true;
+// OpenAPI.TOKEN = async () => localStorage.getItem('access_token') ?? '';
+OpenAPI.HEADERS = async () => ({ 'X-Client': 'web' });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
