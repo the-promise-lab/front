dc56ad141317aeed88e26ec65234a05f975da41c
sungpaks <emforhs03150@gmail.com>
2025-08-09 14:39:16 +0900

    config: TanStack Query 쿼리클라이언트, ESLint 설정

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import tanstackQuery from '@tanstack/eslint-plugin-query';
 
 export default tseslint.config([
   {
@@ -16,6 +17,12 @@ export default tseslint.config([
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
+    plugins: {
+      '@tanstack/query': tanstackQuery,
+    },
+    rules: {
+      ...tanstackQuery.configs.recommended.rules,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App.tsx';
 import './styles/input.css';
 import { OpenAPI } from './api/core/OpenAPI';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 /* OpenAPI 전역설정 */
 OpenAPI.BASE = import.meta.env.VITE_API_BASE_URL;
@@ -11,8 +12,12 @@ OpenAPI.BASE = import.meta.env.VITE_API_BASE_URL;
 // OpenAPI.TOKEN = async () => localStorage.getItem('access_token') ?? '';
 OpenAPI.HEADERS = async () => ({ 'X-Client': 'web' });
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
