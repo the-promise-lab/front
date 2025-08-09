004ed92f2251dc0faf12f2843a90af8e5142833b
sungpaks <emforhs03150@gmail.com>
2025-08-09 13:52:34 +0900

    lint: lint-staged + husky 룰 개선

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
-  const a = null;
-  const b = null;
 
   return (
     <>
@@ -30,8 +28,6 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <div>foo</div>
-      <div>bar</div>
     </>
   );
 }
