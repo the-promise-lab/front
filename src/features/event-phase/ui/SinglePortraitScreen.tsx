e86821720b0ae643b82868fa99a7ee62fa83cfe3
sungpaks <emforhs03150@gmail.com>
2025-09-21 22:00:53 +0900

    update: 대사창에서 새로운 텍스트에 대해 일관적으로 타이핑효과 적용

--- src/features/event-phase/ui/SinglePortraitScreen.tsx
+++ src/features/event-phase/ui/SinglePortraitScreen.tsx
@@ -1,10 +1,26 @@
+import { useState, type MouseEvent } from 'react';
 import PortraitBanner from './kit/PortraitBanner';
 
+const portraits = [
+  '우리는 통장에 돈이 빠지는게 더 낫지. 근손실보다는..',
+  '맞습니다 헴!!',
+  '닭가슴살 또 챙기러 갑시다!!',
+];
+
 export default function SinglePortraitScreen() {
+  const [portraitIndex, setPortraitIndex] = useState(0);
+  const handleNextPortrait = (e: MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    setPortraitIndex(prev => (prev + 1) % portraits.length);
+  };
+  const currentPortrait = portraits[portraitIndex];
   return (
-    <PortraitBanner
-      portrait='우리는 통장에 돈이 빠지는게 더 낫지. 근손실보다는..'
-      characterName='캐릭터 이름'
-    />
+    <div className='relative h-full w-full' onClick={handleNextPortrait}>
+      <PortraitBanner
+        key={`currentPortrait-${portraitIndex}`}
+        portrait={currentPortrait}
+        characterName='캐릭터 이름'
+      />
+    </div>
   );
 }
